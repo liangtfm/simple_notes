5e6f188eeab9799a93965f1962124c03cb669f5d
Anthony+Sarah <Anthony+Sarah@appacademy.io>
2014-02-11 08:59:28 -0500

    Initial commit with Backbone and updated Gemfile

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.14'
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+end
+
+gem 'jquery-rails'
+gem 'backbone-on-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JournalApp::Application.config.secret_token = '3c29e85e0ba185f80074c37f752c90af44dc58702d59988b9e05a9f4843f9b4108fc91a4f1d36a737988338f68ebe5c266e806e53c2f643796edb0482c210b73'
